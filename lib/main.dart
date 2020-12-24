1497f01f0869795bf9114e4006b04cde2edc0be6
AD0791 <alexdisla1991@gmail.com>
2020-12-24 14:38:32 -0500

    refactoring the stateful widget list transactions is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:bud_plan/models/transaction.dart';
+import 'package:bud_plan/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,18 +15,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(id:"1", 
-      title: "Barner Glass", 
-      amount: 59.99, 
-      date: DateTime.now()
-      ),
-      Transaction(id:"2", 
-      title: "Razoire Electric", 
-      amount: 19.99, 
-      date: DateTime.now()
-      ),
-  ];
 
   final titleController = TextEditingController();
   final amountController= TextEditingController();
@@ -78,54 +65,7 @@ class MyHomePage extends StatelessWidget {
             ),
             elevation: 15,  
           ),
-          Column(
-            children: transactions.map((taction) => 
-              Card(child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text('\$: ${taction.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.purple 
-                          ),
-                      ),
-                    ),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                        Text(
-                          taction.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25
-                          ),  
-                        ),
-                        Text(
-                          DateFormat('yyyy/MM/dd').format(taction.date),
-                          style: TextStyle(
-                            color: Colors.grey
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                elevation: 15,
-              )
-            ).toList(),
-          ),
+          TransactionList(),
         ],
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,7 @@
 import 'package:bud_plan/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 class TransactionList extends StatefulWidget {
   @override
   _TransactionListState createState() => _TransactionListState();
@@ -7,23 +9,59 @@ class TransactionList extends StatefulWidget {
 
 class _TransactionListState extends State<TransactionList> {
   final List<Transaction> _userTransactions = [
-    Transaction(id:"1", 
-      title: "Barner Glass", 
-      amount: 59.99, 
-      date: DateTime.now()
-      ),
-      Transaction(id:"2", 
-      title: "Razoire Electric", 
-      amount: 19.99, 
-      date: DateTime.now()
-      ),
+    Transaction(
+        id: "1", title: "Barner Glass", amount: 59.99, date: DateTime.now()),
+    Transaction(
+        id: "2",
+        title: "Razoire Electric",
+        amount: 19.99,
+        date: DateTime.now()),
   ];
-  
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: _userTransactions
+          .map((taction) => Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$: ${taction.amount}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.purple),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          taction.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                        Text(
+                          DateFormat('yyyy/MM/dd').format(taction.date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                elevation: 15,
+              ))
+          .toList(),
     );
   }
 }
