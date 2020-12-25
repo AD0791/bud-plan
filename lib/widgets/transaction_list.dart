f59d41a25d3dd4b20a6c0f5cff6031995af8caef
AD0791 <alexdisla1991@gmail.com>
2020-12-25 14:07:00 -0500

    features: scrollability is now optimize

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,53 +9,50 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // set the height for  the scrollview situation
-      height: 400,
-      child: SingleChildScrollView(
-              child: Column(
-          children: transactions
-              .map((taction) => Card(
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin:
-                              EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            '\$: ${taction.amount}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Colors.purple),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              taction.title,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 25),
-                            ),
-                            Text(
-                              DateFormat('yyyy/MM/dd').format(taction.date),
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      ],
+        // set the height for  the scrollview situation
+        height: 400,
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
                     ),
-                    elevation: 15,
-                  ))
-              .toList(),
-        ),
-      ),
-    );
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$: ${transactions[index].amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                      Text(
+                        DateFormat('yyyy/MM/dd').format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              elevation: 15,
+            );
+          },
+          itemCount: transactions.length,
+        ));
   }
 }
