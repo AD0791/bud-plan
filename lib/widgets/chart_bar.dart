d61824149e81d5c7bb4693f9a9bbc349d343fe67
AD0791 <alexdisla1991@gmail.com>
2020-12-27 17:30:56 -0500

    bugfix| nou ranger bar yo nan lod et nou ranger koze lajan

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -10,8 +10,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-          child: Text('\$${sAmount.toStringAsFixed(0)}')),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('\$${sAmount.toStringAsFixed(0)}')),
+        ),
         SizedBox(
           height: 5,
         ),
