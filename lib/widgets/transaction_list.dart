be3c9ca00d5809b2e1ba52de8b5da59a740f82d3
AD0791 <alexdisla1991@gmail.com>
2020-12-27 17:23:01 -0500

    refactor | ListTile is better widget to handle the tx list along with the Card widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,67 +11,90 @@ class TransactionList extends StatelessWidget {
     return Container(
         // set the height for  the scrollview situation
         height: 400,
-        child: transactions.isEmpty ? 
-        
-        Column(
-          children: <Widget>[
-            Text("No transaction added yet",
-              style: Theme.of(context).textTheme.title,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 300,
-              child: Image.asset("assets/images/image/waiting.png",
-                fit: BoxFit.cover,
-              ),
-            )
-          ]
-        )
-        
-        : ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$: ${transactions[index].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Theme.of(context).primaryColor
-                      ),
-                    ),
+        child: transactions.isEmpty
+            ? Column(children: <Widget>[
+                Text(
+                  "No transaction added yet",
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 300,
+                  child: Image.asset(
+                    "assets/images/image/waiting.png",
+                    fit: BoxFit.cover,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
+                )
+              ])
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  // return Card(
+                  //   child: Row(
+                  //     children: <Widget>[
+                  //       Container(
+                  //         margin: EdgeInsets.symmetric(
+                  //             vertical: 10, horizontal: 15),
+                  //         decoration: BoxDecoration(
+                  //           border: Border.all(
+                  //             color: Theme.of(context).primaryColor,
+                  //             width: 2,
+                  //           ),
+                  //         ),
+                  //         padding: EdgeInsets.all(10),
+                  //         child: Text(
+                  //           '\$: ${transactions[index].amount.toStringAsFixed(2)}',
+                  //           style: TextStyle(
+                  //               fontWeight: FontWeight.bold,
+                  //               fontSize: 15,
+                  //               color: Theme.of(context).primaryColor),
+                  //         ),
+                  //       ),
+                  //       Column(
+                  //         crossAxisAlignment: CrossAxisAlignment.start,
+                  //         children: <Widget>[
+                  //           Text(
+                  //             transactions[index].title,
+                  //             style: Theme.of(context).textTheme.title,
+                  //           ),
+                  //           Text(
+                  //             DateFormat('yyyy/MM/dd')
+                  //                 .format(transactions[index].date),
+                  //             style: TextStyle(color: Colors.grey),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     ],
+                  //   ),
+                  //   elevation: 15,
+                  // );
+                  return Card(
+                    elevation: 15,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: FittedBox(
+                              child: Text(
+                            '\$: ${transactions[index].amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,),
+                          )),
+                        ),
+                      ),
+                      title: Text(
                         transactions[index].title,
                         style: Theme.of(context).textTheme.title,
                       ),
-                      Text(
-                        DateFormat('yyyy/MM/dd').format(transactions[index].date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              elevation: 15,
-            );
-          },
-          itemCount: transactions.length,
-        ));
+                      subtitle: Text(
+                          DateFormat.yMMMd().format(transactions[index].date)),
+                    ),
+                  );
+                },
+                itemCount: transactions.length,
+              ));
   }
 }
