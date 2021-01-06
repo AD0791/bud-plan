4bf51597181a91d336f36d18d52f18367d9495ba
AD0791 <alexdisla1991@gmail.com>
2021-01-06 15:53:35 -0500

    typo | minor refactoring
    we fix some unnecessary comments

--- lib/main.dart
+++ lib/main.dart
@@ -57,15 +57,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransactions = [
-    // Transaction(
-    //     id: "1", title: "Barner Glass", amount: 59.99, date: DateTime.now()),
-    // Transaction(
-    //     id: "2",
-    //     title: "Razoire Electric",
-    //     amount: 19.99,
-    //     date: DateTime.now()),
-  ];
+  final List<Transaction> _userTransactions = [];
 
   bool _showChart = false;
 
